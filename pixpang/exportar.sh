fc95208a96fadab0236b6a4bfb4e8a9e00bed98e
panreyes <panreyes@panreyes.com>
2011-04-11 18:38:56 +0000

    Prueba de commit desde consola

---
+++ pixpang/exportar.sh
@@ -0,0 +1,36 @@
+@echo off
+cd src
+echo Compilando...
+..\..\bennu-win\bgdc pixpang.prg
+move pixpang.dcb ..
+cd ..
+echo Exportando...
+mkdir export
+mkdir export\fpg
+mkdir export\ogg
+mkdir export\ogg\monstruos
+mkdir export\fnt
+mkdir export\wav
+mkdir export\tour
+mkdir export\fondos
+mkdir export\fondos\monstruos
+copy fpg\*.fpg export\fpg
+copy ogg\*.ogg export\ogg
+copy ogg\monstruos\*.* export\ogg\monstruos
+copy wav\*.wav export\wav
+copy fnt\*.fnt export\fnt
+copy tour\*.pang export\tour
+copy fondos\*.png export\fondos
+copy fondos\monstruos\*.png export\fondos\monstruos
+copy ..\bennu-win\*.dll export
+copy ..\bennu-win\bgdi.exe export\pixpang.exe
+copy pixpang.dcb export
+cd export
+echo DEBES ELEGIR EL PIXpang.EXE
+..\..\bennu-win\pakator 
+move pixpang_exe_pak.exe ..\
+cd ..
+rd /s /q export
+echo LISTO!!
+exit
+pause
