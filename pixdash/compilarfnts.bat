312cb7c393ff41508caa3e70c1e34a20071f0dcb
panreyes <panreyes@panreyes.com>
2012-07-06 16:36:44 +0000

    Elimino los bgdc.imp Revierto la generación de fuentes en pixdash, aún no está lista.

--- pixdash/compilarfnts.bat
+++ pixdash/compilarfnts.bat
@@ -3,6 +3,6 @@ mkdir fnt
 ECHO CREANDO FPGS...
 cd fnt-sources
 copy ..\..\utils\pxlfnt.dcb . /y
-..\..\bennu-win\bgdi pxlfnt 8 fuente_peq
+rem ..\..\bennu-win\bgdi pxlfnt 8 fuente_peq
 del /f pxlfnt.dcb
 pause
