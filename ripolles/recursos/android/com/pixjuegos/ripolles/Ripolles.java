957d024123e78be9b42f6c1c753f8f1cfcc049b9
panreyes <panreyes@panreyes.com>
2012-12-23 00:30:39 +0000

    Ripollés Aña... actualizado! 3.0

---
+++ ripolles/recursos/android/com/pixjuegos/ripolles/Ripolles.java
@@ -0,0 +1,18 @@
+package com.pixjuegos.ripolles;
+
+import org.libsdl.app.SDLActivity;
+import android.os.*;
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */ 
+
+public class Ripolles extends SDLActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
