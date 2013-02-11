5ef5a4fb9eca2b01fb73f040520b940b528cf7b4
panreyes <panreyes@panreyes.com>
2013-02-11 19:16:33 +0000

    Arcade: run.sh keeps gettin better!

--- arcade/run.sh
+++ arcade/run.sh
@@ -3,14 +3,14 @@ export PATH=$PWD/bennu-linux:$PATH
 export LD_LIBRARY_PATH=$PWD/bennu-linux/lib:$LD_LIBRARY_PATH
 
 cd arcade
-$next=`bgdi arcade`
+next=`bgdi arcade`
 cd ..
 
-echo Next: %next%
+echo Next: $next
 
-if [ "$next" != "salir" ]; then exit; fi
+if ["$next"=="salir"]; then exit; fi
 cd $next
 bgdi $next arcade
 cd ..
 
-sh run.sh
+./run.sh
