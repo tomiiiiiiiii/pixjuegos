57326ba0b1b6606db59a9985a40246ed6581986a
panreyes <panreyes@panreyes.com>
2013-02-11 17:14:25 +0000

    Arcade: run.sh Para linux

---
+++ arcade/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export PATH=$PWD/bennu-linux:$PATH
+export LD_LIBRARY_PATH=$PWD/bennu-linux/lib:$LD_LIBRARY_PATH
+
+cd arcade
+$next=`bgdi arcade`
+cd ..
+
+echo Next: %next%
+
+if [ "$next" != "salir" ]; then exit; fi
+cd $next
+bgdi $next arcade
+cd ..
+
+sh run.sh
