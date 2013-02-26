6bebcd79ba5c47bd782fff0893f9f93702db41c8
panreyes <panreyes@panreyes.com>
2013-02-26 14:42:28 +0000

    Los fpgs de PiXDash deben ser siempre exportados a 16 bits

--- pixdash/compilarfpgs.bat
+++ pixdash/compilarfpgs.bat
@@ -1,4 +1,4 @@
 @echo off
 set bits=%1
-if "%bits%"==""; set bits=32
+if "%bits%"==""; set bits=16
 call ..\scripts\compilarfpgs.bat %bits% enemigos menu powerups pix pux pax pex moneda tiles tiles2 tiles3 tiles4 tiles5 premios general
