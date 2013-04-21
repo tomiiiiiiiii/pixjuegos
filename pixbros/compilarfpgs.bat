21f91ce5fa0a1050c21b425a47453cbeec15ca02
panreyes <panreyes@panreyes.com>
2013-04-21 10:47:11 +0000

    PiXBros: Actualizado Compilarfpgs

--- pixbros/compilarfpgs.bat
+++ pixbros/compilarfpgs.bat
@@ -1,4 +1,5 @@
 @echo off
 set bits=%1
 if "%bits%"==""; set bits=32
-call ..\scripts\compilarfpgs.bat %bits% enemigos general items jefes pax pix pux intro-de intro-en intro-es intro-fr intro-it intro-jp menu menu-de menu-en menu-es menu-fr menu-it menu-jp intro
+REM call ..\scripts\compilarfpgs.bat %bits% enemigos general items jefes pax pix pux intro-de intro-en intro-es intro-fr intro-it intro-jp menu menu-de menu-en menu-es menu-fr menu-it menu-jp intro
+call ..\scripts\compilarfpgs.bat %bits% enemigos general items jefes pax pix pux intro menu
